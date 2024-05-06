8ec31868432c7f29f4c909111ad5536265aa0509
yassine <yassineseghr17@gmail.com>
2024-05-06 14:55:22 -0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,21 +1,6 @@
 /*! \file   main.c
- *  \brief  Custom Bootloader
- *  \author William Huang
- *  \date   2017-10-28
- *  \copyright Copyright (C) 2017 William Huang
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- * 
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- * 
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
+*/
+
 
 #include "stm32f4xx.h"                  // Device header
 #include "HAL_GPIO_Driver.h"
