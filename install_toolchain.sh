759c1f1662971cb19c2275243b06d0a948b4e3d3
yassine <yassineseghr17@gmail.com>
2024-05-08 04:34:56 -0600

    Update install_toolchain.sh
    symlinking to /usr/bin instead of just copying the toolchain

--- install_toolchain.sh
+++ install_toolchain.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# check if the toolchain is already installed
-if [ -x "$(command -v arm-none-eabi-gcc)" ]; then
-    echo "Toolchain is already installed."
-    exit 0
-fi
-
 # variables
 toolchain="gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2"
 path="https://developer.arm.com/-/media/Files/downloads/gnu-rm/10.3-2021.10/"
@@ -18,23 +12,24 @@ echo "Extracting the tarball..."
 # extract the tarball
 tar -xvf $toolchain > /dev/null
 
-# ask to move the toolchain to /opt/
-echo "Do you want to move the toolchain to /opt/ ?"
-read -p "y/n: " move
-if [ "$move" == "y" ]; then
-    sudo mv gcc-arm-none-eabi-10.3-2021.10 /opt/
-    echo "Toolchain moved to /opt/"
-fi
-# ask to copy the toolchain to /usr/bin/
-echo "Do you want to copy the toolchain to /usr/bin/ ?"
-read -p "y/n: " copy
-if [ "$copy" == "y" ]; then
-    sudo cp gcc-arm-none-eabi-10.3-2021.10/bin/arm-none-eabi-* /usr/bin/
-    echo "Toolchain copied to /usr/bin/"
-fi
+#  move the toolchain to /usr/share
+echo "moving toolchain to /usr/share"
+sudo mv gcc-arm-none-eabi-10.3-2021.10 /usr/share/
+echo "Toolchain moved to /usr/share"
+
+# symlinking
+echo "Creating symlinks..."
+sudo ln -s /usr/share/gcc-arm-none-eabi-10.3-2021.10/bin/*  /usr/bin
+echo "Done!"
+
 
 # clean up
-rm -rf gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2 gcc-arm-none-eabi-10.3-2021.10
+echo "Would you like to clean up the files?"
+read -p "y/n: " clean
+if [ "$clean" == "y" ]; then
+    rm -rf gcc-arm-none-eabi-10.3-2021.10-x86_64-linux.tar.bz2 gcc-arm-none-eabi-10.3-2021.10
+    echo "Files cleaned up!"
+fi
 
 # check if the toolchain is installed
 arm-none-eabi-gcc --version
